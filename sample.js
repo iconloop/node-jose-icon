472a94b134432ae0baa2db7bce9d854fce938089
windies21 <winDy@windystudio.com>
2021-10-27 09:32:13 +0900

    [SC-293] Remove email from ValidateVCRequest keys. (#2)
    * [SC-293] Remove email from ValidateVCRequest keys.
    
    * 0.1.1
    
    * 0.0.2

--- sample.js
+++ sample.js
@@ -3,10 +3,10 @@
 const pds_client = require('node-jose-icon').icon;
 
 data = [
-    ["a@abc.com", "01012345678"],
-    ["b@abc.com", "01011115678"],
-    ["c@abc.com", "01022225678"],
-    ["d@abc.com", "01033335678"]
+    ["01012345678"],
+    ["01011115678"],
+    ["01022225678"],
+    ["01033335678"]
 ]
 
 pds_client.serverCheck()
@@ -20,9 +20,9 @@ pds_client.validation(data, function (response) {
 // Request Validation to PDS.
 // {"status":"PDS is running..!"}
 // request with  [
-//   [ 'a@abc.com', '01012345678' ],
-//   [ 'b@abc.com', '01011115678' ],
-//   [ 'c@abc.com', '01022225678' ],
-//   [ 'd@abc.com', '01033335678' ]
+//   [ '01012345678' ],
+//   [ '01011115678' ],
+//   [ '01022225678' ],
+//   [ '01033335678' ]
 // ]
 // response is  {"iat": 1628090290, "result": [1, 1, 1, 1], "type": "VALIDATE_VC_RESPONSE"}
